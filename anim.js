e3f97fab3b54a21b225f12a95ff4ea7f4e238fe6
Florin Toader <florin.toader@magnoliabox.com>
2019-08-30 19:23:23 +0300

    Added a bunch of functionality.

--- anim.js
+++ anim.js
@@ -1,53 +1,86 @@
 (function () {
-  puppeteer = {};
-  puppeteer.animations = {};
-  puppeteer._allGroups = [];
-  puppeteer._allElements = [];
-  puppeteer._allGroupElements = [];
-  puppeteer._allElementsSet = new Set();
-  puppeteer._windowOffset = 0;
-  puppeteer._scrollUp = false;
+  const puppeteer = {
+    animations: {},
+    _allGroups: [],
+    _allElements: [],
+    _allGroupElements: [],
+    _allElementsSet: new Set(),
+    _windowOffset: 0,
+    _scrollUp = false
+  }
 
-  puppeteer._update = function () {
-    puppeteer._allElementsSet.forEach(function (elem) {
-      var rect = elem.getBoundingClientRect();
-      var anim = elem.getAttribute('p-animation');
-      var animMethod = puppeteer.snakeToCamel(anim);
+  const snakeToCamel = (s) => {
+    return s.replace(/(\-\w)/g, (m) => { 
+      return m[1].toUpperCase() 
+    })
+  }
+
+  const animationStart = (e) => {
+    e.target.animationRunning = true
+    // console.log('Animation start', e)
+  }
+
+  const animationEnd = (e) => {
+    e.target.animationRunning = false
+    // console.log('Animation end', e)
+  }
+  
+  const showPercent = window.setInterval(() => {
+    if (currentPercent < 100) {
+      currentPercent += 1
+    } else {
+      currentPercent = 0
+    }
+  }, 40)
+
+  const update = () => {
+    puppeteer._allElementsSet.forEach((elem) => {
+      const rect = elem.getBoundingClientRect()
+      const anim = elem.getAttribute('p-animation') 
 
       if (rect.top < window.innerHeight && rect.top > 0 && !puppeteer._scrollUp) {
-        if (!elem.visible) {
-          eval(`puppeteer.animations.${animMethod}(elem)`);
-          elem.visible = true;
+        if (!elem.visible) {          
+          window.setTimeout(() => {
+            elem.style.animation = `${anim} 1s 1`
+            elem.animationStart = animationStart
+            elem.animationEnd = animationEnd
+            elem.animationIteration = animationIteration
+          })
+
+          elem.visible = true
         }
       } else if (puppeteer._scrollUp && rect.top > window.innerHeight) {
-        elem.visible = false;
+        elem.style.animation = undefined
+        elem.visible = false
       }
-    });
+    })
   }
 
-  document.addEventListener("DOMContentLoaded", function () {
-    puppeteer._allElements = document.querySelectorAll('[p-animation]');
-    puppeteer._allElements.forEach(function (e) {
-      puppeteer._allElementsSet.add(e);
-    });
+  document.addEventListener("DOMContentLoaded", () => {
+    window.setInterval(tick, 500)
 
-    // puppeteer._update();
-  });
+    puppeteer._allElements = document.querySelectorAll('[p-animation]')
+    puppeteer._allElements.forEach((e) => {
+      puppeteer._allElementsSet.add(e)
+      e.addEventListener('animationstart', animationStart)
+      e.addEventListener('animationend', animationEnd)      
+    })
 
-  document.addEventListener('scroll', function (e) {
-    console.log('Scroll')
+    update()
+  })
 
+  tick = () => {
+    console.log('Tick')
+  }
+
+  document.addEventListener('scroll', (e) => {
     if (window.pageYOffset < puppeteer._windowOffset) {
-      puppeteer._scrollUp = true;
+      puppeteer._scrollUp = true
     } else {
-      puppeteer._scrollUp = false;
+      puppeteer._scrollUp = false
     }
 
-    puppeteer._windowOffset = window.pageYOffset;
-    // puppeteer._update();
-  });
-
-  puppeteer.snakeToCamel = function (s) {
-    return s.replace(/(\-\w)/g, function (m) { return m[1].toUpperCase(); });
-  }
-})();
+    puppeteer._windowOffset = window.pageYOffset
+    update()
+  })
+})()
